904671183d7009d7981698df42a671465525689e
yosuke1234567 <yosuke1234567>
2025-11-02 21:55:33 +0900

    update

--- src/Time.tsx
+++ src/Time.tsx
@@ -2,7 +2,7 @@ const { Image, AutoLayout, Text } = figma.widget;
 
 export const Time = ({ time }: { time: any }) => {
   const presentation = time.presentations[0];
-  if (!presentation.category.match(/^.*(講演|基調|セッション).*$/)) {
+  if (!presentation.presenters.length) {
     return null;
   }
   return (
@@ -12,7 +12,9 @@ export const Time = ({ time }: { time: any }) => {
         {presentation.presenters.map((presenter: any, i: number) => (
           <AutoLayout key={i} direction="vertical">
             <Image src={presenter.photo} width={80} height={80} />
-            <MyText>{presenter.name}</MyText>
+            <MyText fontWeight={700}>{presenter.company_name}</MyText>
+            <MyText>{presenter.post.replace(/<br\s*\/?>/g, "\n")}</MyText>
+            <MyText fontWeight={700}>{presenter.name}</MyText>
           </AutoLayout>
         ))}
       </AutoLayout>
@@ -20,6 +22,8 @@ export const Time = ({ time }: { time: any }) => {
   )
 }
 
-const MyText = ({ children }: { children: string }) => (
-  <Text fontFamily="Inter" fontWeight={400}>{children}</Text>
-);
+const MyText = ({ fontSize = 16, fontWeight = 400, children }: { fontSize?: number; fontWeight?: WidgetJSX.FontWeight; children: string }) => (
+  <Text fontFamily="Inter" fontSize={fontSize} fontWeight={fontWeight}>
+    {children}
+  </Text>
+)
